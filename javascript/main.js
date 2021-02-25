d7ea318a4cf33781a9742ec8217e46a2efb6c8de
Emilio94 <emilio.994@gmail.com>
2021-02-25 13:48:20 +0100

    Più metodi

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,8 @@
 // Come faccio a sapere se un numero è divisibile per?
 // Abbiamo visto qualcosa di particolare che possiamo usare?
 
+// Metodo For
+console.log("Metodo For");
 for (var i = 0; i <= 100; i++) {
     if (i % 15 == 0 && i != 0) {
         console.log("FizzBuzz");
@@ -20,8 +22,48 @@ for (var i = 0; i <= 100; i++) {
         console.log(i);
     }
 }
+console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+console.log("");
 
+// Metodo While
+console.log("Metodo While");
+var i = 0;
+while (i <= 100) {
+    if (i % 15 == 0 && i != 0) {
+        console.log("FizzBuzz");
+    }
+    else if ((i % 3 == 0 || i % 15 == 0) && i != 0) {
+        console.log("Fizz");
+    }
+    else if ((i % 5 == 0 || i % 15 == 0) && i != 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+    i++ 
+}
+console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+console.log("");
 
+// Metodo Do-While
+console.log("Metodo Do-While");
+var i = 0;
+do {
+    if (i % 15 == 0 && i != 0) {
+    console.log("FizzBuzz");
+    }
+    else if ((i % 3 == 0 || i % 15 == 0) && i != 0) {
+        console.log("Fizz");
+    }
+    else if ((i % 5 == 0 || i % 15 == 0) && i != 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+    i++     
+} while (i <= 100);
 
 
 
