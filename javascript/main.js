6e21e7137de90876c94310b068e91fe4fd7ff8df
Emilio94 <emilio.994@gmail.com>
2021-02-25 14:04:03 +0100

    Più metodi

--- javascript/main.js
+++ javascript/main.js
@@ -32,10 +32,10 @@ while (i <= 100) {
     if (i % 15 == 0 && i != 0) {
         console.log("FizzBuzz");
     }
-    else if ((i % 3 == 0 || i % 15 == 0) && i != 0) {
+    else if ((i % 3 == 0 && i % 15 != 0) && i != 0) {
         console.log("Fizz");
     }
-    else if ((i % 5 == 0 || i % 15 == 0) && i != 0) {
+    else if ((i % 5 == 0 && i % 15 != 0) && i != 0) {
         console.log("Buzz");
     }
     else {
